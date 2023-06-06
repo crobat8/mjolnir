b53607b22b67734102384ad3e4f9024c4fa914f4
crobat8 <133931076+crobat8@users.noreply.github.com>
2023-06-05 22:19:25 -0700

    save spot before working on lining up days on graphs

--- src/componets/Graph.jsx
+++ src/componets/Graph.jsx
@@ -31,16 +31,20 @@ const Graph = (props) =>{
 
     const{currentUser} = useContext(AuthContext);
 
-    const[loading,setLoading] = useState(true);
+    const[loading1,setLoading1] = useState(true);
+
+    const[loading2,setLoading2] = useState(true);
 
     const[workouts,setWorkouts] = useState([]);
+
+    const[lifts,setLifts]=useState([]);
     
     function getWorkouts(){
         const workoutCollenctionRef =collection(db,"workouts")
-        const sifted = query(workoutCollenctionRef
+        const siftedW = query(workoutCollenctionRef
             ,where("uid","==",currentUser.uid)
             ,where("event" ,"==",props.event));
-        getDocs(sifted)
+        getDocs(siftedW)
         .then(response =>{
             
             const work =  response.docs.map(doc =>({
@@ -48,10 +52,29 @@ const Graph = (props) =>{
                 id: doc.id,
             }))
             setWorkouts(work)
-            setLoading(false);
-        })
+            setLoading1(false);
+        }
+        )
+        .catch(error => console.error(error.message))
+
+
+        const siftedL = query(workoutCollenctionRef
+            ,where("uid","==",currentUser.uid)
+            ,where("event" ,"==","Lift"));
+        getDocs(siftedL).then(response =>{
+            
+            const Lift =  response.docs.map(doc =>({
+                data: doc.data(),
+                id: doc.id,
+            }))
+            setLifts(Lift)
+            setLoading2(false);
+        }
+        
+        )
         .catch(error => console.error(error.message))
     }
+    
 
     function Display(){
         //console.log(workouts[0].data[props.important])
@@ -60,13 +83,23 @@ const Graph = (props) =>{
         let gatheredInfoTwo   = [];
         let gatheredInfoThree = [];
         let gatheredDates = [];
+        let gatheredLiftsOne = [];
+        let gatheredLiftsTwo = [];
+        var combinedDay = 0;
+
+        
         for(var i = 0;i<workouts.length;i++){
             gatheredInfoOne[workouts.length-1-i] = workouts[i].data[props.throwOne];
             gatheredInfoTwo[workouts.length-1-i] = workouts[i].data[props.throwTwo];
             gatheredInfoThree[workouts.length-1-i] = workouts[i].data[props.throwThree];
             gatheredDates[workouts.length-1-i] = workouts[i].data.day;
         }
-        
+
+        for(var x = 0;x<lifts.length;x++){
+            gatheredLiftsOne[x] = lifts[x].data.WeightOne;
+            gatheredLiftsTwo[x] = lifts[x].data.WeightTwo;
+        }
+
         const data = {
             labels: gatheredDates,
             datasets: [{
@@ -75,44 +108,58 @@ const Graph = (props) =>{
                     backgroundColor: 'aqua',
                     borderColor: 'black',
                     pointBorder: 'green',
+                    yAxisID : 'Distance',
                 },{
                     label: props.throwTwo,
                     data: gatheredInfoTwo,
                     backgroundColor: 'red',
                     borderColor: 'black',
                     pointBorder: 'green',
+                    yAxisID : 'Distance',
                 },{
                     label: props.throwThree,
                     data: gatheredInfoThree,
                     backgroundColor: 'yellow',
                     borderColor: 'black',
                     pointBorder: 'green',
+                    yAxisID : 'Distance',
+                },{
+                    label: "primary Lift",
+                    data: gatheredLiftsOne,
+                    backgroundColor: 'green',
+                    borderColor: 'black',
+                    pointBorder: 'green',
+                    yAxisID : 'Weight',
+                    showLine: false
+                },{
+                    label: "Secondary Lift",
+                    data: gatheredLiftsTwo,
+                    backgroundColor: 'brown ',
+                    borderColor: 'black',
+                    pointBorder: 'green',
+                    yAxisID : 'Weight',
+                    showLine: false
                 }
             ]
         }
+
         
-        /*const options ={
+        const options ={
             responsive: true,
-            plugins:{
-                legend:true,
-                drawHorizontalLine: {
-                lineY: [55, 55],
-                lineColor: "rgba(50, 155, 255, 0.85)",
-                text: 'Obj 67 & 68',
-                textPosition: 720,
-                textFont: '18px sans-serif',
-                textColor: "rgba(50, 155, 255, 0.85)"
-                },
-            },
             
+
             scales:{
-                y:{
-                    min: parseInt(props.domainMin) ,
-                    max: parseInt(props.domainMax),
+                Distance:{
+                    
+                    position: 'left',
+                },
+                Weight:{
+                    
+                    position: 'right',
                 }
             }
-        }*/
-
+        }
+        console.log("test4");
         return(
             <div>
                <h2>
@@ -120,26 +167,8 @@ const Graph = (props) =>{
                 </h2>
                 <Line 
                     data={data}
-                    options={{
-                        plugins: {
-                          title: {
-                            display: true,
-                            text: "Users Gained between 2016-2020"
-                          },
-                          legend: {
-                            display: false
-                          },
-                          drawHorizontalLine: {
-                            lineY: [55, 55],
-                            lineColor: "rgba(50, 155, 255, 0.85)",
-                            text: 'Obj 67 & 68',
-                            textPosition: 720,
-                            textFont: '18px sans-serif',
-                            textColor: "rgba(50, 155, 255, 0.85)"
-                            },
-                        }
-                    }}
-                    
+                    options={options}
+
                 ></Line> 
             </div>
             
@@ -149,10 +178,10 @@ const Graph = (props) =>{
     }
 
     useEffect(() => {
-        getWorkouts()
+        getWorkouts();
     },[])
 
-    if(loading){
+    if(loading1 || loading2){
         return <h1>loading data</h1>;
     }
 
